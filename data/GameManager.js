781d44312d61cf3c568eaac7bcd32b35b5acf805
predadorBR <151166110+predadorBR@users.noreply.github.com>
2024-04-14 17:16:57 -0300

    Update GameManager.js

--- data/GameManager.js
+++ data/GameManager.js
@@ -44,7 +44,7 @@ class EJS_GameManager {
                           //'snes9x_gfx_transp = "enabled"\n'+
                           //'snes9x_randomize_memory = "enabled"\n'+
                           //'snes9x_reduce_sprite_flicker = "enabled"\n'+
-                          //'snes9x_overscan = "enabled"\n';
+                          'snes9x_overscan = "enabled"\n';
         this.writeFile("/home/web_user/retroarch/userdata/config/Snes9x/Snes9x.opt", snes9x_opcoes);
         
         this.mkdir("/data");
